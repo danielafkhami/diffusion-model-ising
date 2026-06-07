4ce075d6052326d62facbf24cddd45112abf7c2c
Daniel Afkhami <dan.afkhami@gmail.com>
2026-06-07 17:15:49 -0400

    Add fixed seed and generated dataset

--- dataset_generator.py
+++ dataset_generator.py
@@ -5,20 +5,22 @@ from ising_grid import generate_ising_grid
 temp = 2.269
 size = 32
 sweeps = 10
-samples = 1000
+samples = 5000
 
 def generate_dataset(num_samples, size, sweeps, beta):
 
+    np.random.seed(42)
     dataset = np.zeros((num_samples, size, size), dtype=np.int8)
 
     for i in range(num_samples):
 
         dataset[i] = generate_ising_grid(size, sweeps, beta)
 
-        if (i + 1) % 10 == 0:
+        if (i + 1) % 100 == 0:
             print(f"Generated {i+1}/{num_samples} samples")
 
     np.save(f"ising_dataset_{size}x{size}.npy", dataset)
+    print("Saved.")
 
 if __name__ == "__main__":
     generate_dataset(samples, size, sweeps, 1/temp)
